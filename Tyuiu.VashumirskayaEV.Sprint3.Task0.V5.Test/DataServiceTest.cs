ec81ebba789bdf6edb3654e358058e9427aaf5f6
Елизавета Вашумирская <Tyuiu.vashumirskayaEV@outlook.com>
2025-11-26 22:31:09 +0500

    Add project files.

---
+++ Tyuiu.VashumirskayaEV.Sprint3.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.VashumirskayaEV.Sprint3.Task0.V5.Lib;
+
+namespace Tyuiu.VashumirskayaEV.Sprint3.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double res = ds.GetSumSeries(1, 10);
+            double wait = 81.083;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VashumirskayaEV.Sprint3.Task0.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.VashumirskayaEV.Sprint3.Task0.V5.Lib;
+
+namespace Tyuiu.VashumirskayaEV.Sprint3.Task0.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Вашумирская Е.В. | СМАРТб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнила: Вашумирская Елизавета Владимировна | СМАРТб-25-1             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда     *");
+            Console.WriteLine("* по формуле: S = сумма от k=1 до 10 (1/sin(k))^2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine($"Старт шага = {startValue}");
+            Console.WriteLine($"Конец шага = {stopValue}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.GetSumSeries(startValue, stopValue);
+
+            Console.WriteLine($"Сумма ряда = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
